3f28a98fcf11495ab1b92ee73df22241b0ddae04
Dhaval Yoganandi <dhaval.yoganandi@vnc.biz>
2015-03-19 01:02:22 -0700

    ZMPKG packaging added. And 1.0.1.0 released.

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+TOPDIR=.
+include $(TOPDIR)/common.mk
+include $(ZIMBRA_BUILD_ROOT)/extensions-extra/zmpkg/mk/main-std.mk
---
+++ conf.mk
@@ -0,0 +1,10 @@
+VERSION=1.0.1.0
+PACKAGE=vnc-ad-password-change
+MAINTAINER=Dhaval Yoganandi <dhaval.yoganandi@vnc.biz>
+SECTION=commercial/zcs-util
+PRIORITY=optional
+DEPENDS=
+DESCRIPTION=ADPassword change extension
+REDMINE_UPLOAD_PROJECT=vnc-ad-password-change
+REDMINE_UPLOAD_URL=https://redmine.vnc.biz/redmine/
+LAST_VERSION_TAG=vnc-ad-password-change-$(LAST_VERSION)
